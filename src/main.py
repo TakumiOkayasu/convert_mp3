7d05bf7ed1ce2b986ea93b951e0c927039d03826
Takumi Okayasu <takumi_okys@icloud.com>
2024-11-13 03:04:39 +0000

    feat: write main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 
 from pathlib import Path
+
 from SoundToText import mp4_to_text
 
 
 def main() -> None:
-    mp4_to_text(Path.cwd() / Path("tests/mp4/動画視聴本講義-01.mp4"))
+    print("処理開始")
+    mp4_to_text(Path.cwd() / Path("tests/mp4/AKE100720.mp4"))
 
 
 if __name__ == "main":
